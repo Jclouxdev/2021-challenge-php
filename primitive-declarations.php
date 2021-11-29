6811806a0bae202933dba422ddccba00535dbf22
Jcloux <joseph.cloux@ynov.com>
2021-11-29 09:35:11 +0100

    v1

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php 
+$firstStr = "String of word";
+$firstEscapeStr = "Retour\n à la ligne";
+$firstBool = true;
+$firstInteger = 11;
+$firstFloat = 11.11;
+$firstNull = null;
+$firstArray =array(2, null, 'Hi', 5.5, false);
+?>
