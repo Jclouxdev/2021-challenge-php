32d7fad54f72c09bd928c8237c81adf828b1f8c6
Jcloux <joseph.cloux@ynov.com>
2021-11-29 10:13:28 +0100

    v4

--- be-polite.php
+++ be-polite.php
@@ -1,10 +1,10 @@
 <?php
 $hour = date('h');
-if ($hour >= 6 && $hour <= 12 ) {
+if ($hour >= 06 && $hour <= 12 ) {
   echo "Hello! Have a nice day :)";
-} else if ($hour >= 12 && $hour <= 18) {
+} elseif ($hour > 12 && $hour <= 18) {
   echo "Have a good afternoon!";
-} else if ($hour >= 18 && $hour <= 21 ) {
+} elseif ($hour > 18 && $hour <= 21 ) {
   echo "Good evening! Hope you had a good day!";
 } else {
   echo "Good night! See you tomorrow :)";
