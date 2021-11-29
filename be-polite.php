19a046eb45b0896921ec01a1ab6ed21e9e7e7067
Jcloux <joseph.cloux@ynov.com>
2021-11-29 10:09:26 +0100

    v3

--- be-polite.php
+++ be-polite.php
@@ -1,11 +1,10 @@
 <?php
-date_default_timezone_set('');
-$hour = date('H');
+$hour = date('h');
 if ($hour >= 6 && $hour <= 12 ) {
   echo "Hello! Have a nice day :)";
-} else if ($hour > 12 && $hour <= 18) {
+} else if ($hour >= 12 && $hour <= 18) {
   echo "Have a good afternoon!";
-} else if ($hour > 18 && $hour <= 21 ) {
+} else if ($hour >= 18 && $hour <= 21 ) {
   echo "Good evening! Hope you had a good day!";
 } else {
   echo "Good night! See you tomorrow :)";
