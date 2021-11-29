f373c91bdcb534d35007db3b67c642cb52bac455
Jcloux <joseph.cloux@ynov.com>
2021-11-29 13:18:12 +0100

    v1

---
+++ string-manipulator.php
@@ -0,0 +1,21 @@
+<?php
+function capsMe($string) : string {
+  return strtoupper($string);
+};
+function lowerMe($string) : string {
+  return strtolower($string);
+};
+function upperCaseFirst($string) : string {
+  return ucwords($string);
+};
+function lowerCaseFirst($string) : string {
+  $explode = explode(" ", $string);
+  $mapped_array = array_map(function($item) {
+    return lcfirst($item);
+  }, $explode);
+  return implode(' ', $mapped_array);
+}
+function removeBlankSpace($string) : string {
+  return str_replace(" ", "", $string);
+}
+?>
