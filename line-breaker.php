94673c97b985eaebe249e86c83337d2f90f9a783
Jcloux <joseph.cloux@ynov.com>
2021-11-30 11:00:11 +0100

    wordwrap

--- line-breaker.php
+++ line-breaker.php
@@ -1,40 +1,7 @@
 <?php
-
 function breakLines($string, $length) : string {
-    $acc = [];
-    $regex = '/ |\n/';
-    //$string_arr = explode(' ', $string);
-    $string_arr = preg_split($regex, $string);
-    $current_line_length = 0;
-
-    while (count($string_arr) > 0) {
-        $word = $string_arr[0];
-
-        // check if word exceeds max line length
-        if (strlen($word) > $length) {
-            throw new Exception('word length greater than max line length');
-        }
-
-        if ($current_line_length + strlen($word) < $length) {
-            // if the current line can handle space + the word
-            // add space + word to final string array
-            // increments current line length
-            // remove treated word from array
-            if(!$current_line_length == 0) {
-              $acc[] = ' ';
-            }
-            $acc[] = $word;
-            $current_line_length += strlen($word);
-            array_splice($string_arr, 0, 1);
-        } else {
-            // otherwise add break line to final string array
-            // then reset line counter cause we start a new one
-            $acc[] = PHP_EOL;
-            $current_line_length = 0;
-        }
-    }
-    return implode('', $acc);
-}
+    return wordwrap($string, $length);
+};
 //echo breakLines("Line with words", 15);
 //echo breakLines("Line with words should break", 15);
 //echo breakLines("Line with words should break at this spot", 15);
