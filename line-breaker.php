9ec16da8a2eba48b617f2a7041cbb5c178afb6b6
Jcloux <joseph.cloux@ynov.com>
2021-11-30 10:43:17 +0100

    -comments

--- line-breaker.php
+++ line-breaker.php
@@ -37,7 +37,7 @@ function breakLines($string, $length) : string {
 }
 //echo breakLines("Line with words", 15);
 //echo breakLines("Line with words should break", 15);
-echo breakLines("Line with words should break at this spot", 15);
-echo PHP_EOL . '-----------------------------------' . PHP_EOL;
-echo breakLines("Title is long\nLine with words break", 12);
+//echo breakLines("Line with words should break at this spot", 15);
+//echo PHP_EOL . '-----------------------------------' . PHP_EOL;
+//echo breakLines("Title is long\nLine with words break", 12);
 ?>
