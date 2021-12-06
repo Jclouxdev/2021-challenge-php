38b28a22638e9e4d73eb4ad18183b19b9c0b2f4e
Jcloux <joseph.cloux@ynov.com>
2021-12-06 16:17:41 +0100

    fix

--- join.php
+++ join.php
@@ -1,6 +1,5 @@
 <?php 
-function joinWords(array $array, string $joinBy) : string {
-  $joinBy = " ";
+function joinWords(array $array, string $joinBy=" ") : string {
   $string = "";
   for($i=0; $i < count($array); $i++) {
     if($i == count($array)-1) {
