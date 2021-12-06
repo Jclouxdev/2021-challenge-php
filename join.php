0339b7bc5b0902ec806ba5f2dd4aa609afe79a92
Jcloux <joseph.cloux@ynov.com>
2021-12-06 15:38:32 +0100

    v1

---
+++ join.php
@@ -0,0 +1,11 @@
+<?php 
+function joinWords(array $array, string $joinBy) {
+  $string = "";
+  for($i=0; $i < count($array); $i++) {
+    if($i == count($array)-1) {
+      $string .= $array[$i];
+    } else $string .= $array[$i].$joinBy;
+  }
+  return $string;
+}
+?>
