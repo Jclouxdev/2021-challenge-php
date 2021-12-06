16f5ff970e289eb1a57b4e4424dcc9bfc85684ec
Jcloux <joseph.cloux@ynov.com>
2021-12-06 16:16:34 +0100

    fix

--- join.php
+++ join.php
@@ -1,6 +1,6 @@
 <?php 
 function joinWords(array $array, string $joinBy) : string {
-  $string = "";
+  $string = " ";
   for($i=0; $i < count($array); $i++) {
     if($i == count($array)-1) {
       $string .= $array[$i];
